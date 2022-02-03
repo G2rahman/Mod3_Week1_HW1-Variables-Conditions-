d21a10ff9760e7ae2d02874a1efb1e807827c7ff
G2rahman <94071678+G2rahman@users.noreply.github.com>
2022-02-03 18:32:49 -0500

    Add files via upload

---
+++ Calc.java
@@ -0,0 +1,62 @@
+class Calc{
+    public static void main(String args[]){
+
+        if (num1>=90)
+        {
+        System.outprintln('A');
+        }
+        else (num1>=80 && num1<90)
+        {
+        System.outprintln('B');
+        }
+        else (num1>=70 && num1<80)
+        {
+        System.outprintln('C');
+        }
+        else (num1>=55 && num1<70)
+        {
+        System.outprintln('D');
+        }
+        else if ()
+        System.outprintln('F');
+
+    int day=1;
+    switch(day) {
+        case 1:
+            week ="Monday";
+        case 2:
+            week="Tuesday";
+        case 3:
+            week="Wednesday";
+        case 4:
+            week="Thursday";
+        case 5:
+            week="Friday";
+        case 6:
+            week="Saturday";
+        case 7:
+            week="Sunday";
+        default:
+            week="N/A";
+    }
+    System.out.println(week);
+
+    }
+    int num1;
+    num1=10;
+
+    if (num1%2)=1
+    {
+        System.out.println(Cool);
+    }
+    if (num1%2)=0 &&  (num>2) || (num<5)
+        System.out.println(Not Cool);
+}
+if (num1%2)=0 &&  (num>6) || (num<20)
+        System.out.println( Cool);
+    if  (num1%2)=0 &&  (num>20)
+    System.out.println( Not Cool);
+
+
+}
+
