c6308f41bf3da99350c1b5491629e45ae337a6f8
G2rahman <94071678+G2rahman@users.noreply.github.com>
2022-02-03 19:28:58 -0500

    Add files via upload

---
+++ Calc.java
@@ -0,0 +1,71 @@
+class Calc{
+    public static void hw1(){
+        int num1=80;
+
+    System.out.println(Grade + "is a" );
+        if (num1>=90)
+        {
+        System.out.println('A');
+        }
+        else if(num1>=80 && int num1<90)
+        {
+        System.out.println('B');
+        }
+        else if(num1>=70 && int num1<80)
+        {
+        System.out.println('C');
+        }
+        else if(num1>=55 && int num1<70)
+        {
+        System.out.println('D');
+        }
+        else 
+        {
+        System.out.println('F');
+        }
+    }
+
+
+    public static void hw2(){
+    int day=1;
+    System.out.println(day);
+    switch(day) {
+        case 1:
+            System.out.println("Monday");
+        case 2:
+            System.out.println("Tuesday");
+        case 3:
+            System.out.println("Wednesday");
+        case 4:
+            System.out.println("Thursday")
+        case 5:
+            System.out.println("Friday")
+        case 6:
+            System.out.println("Saturday");
+
+        case 7:
+            System.out.println("Sunday");
+        default:
+            System.out.println("N/A");
+         }
+    }
+
+    public static void hw3(){
+    int num=10;
+
+    if (num % 2 == 1){
+        System.out.println("Cool");
+    }
+    if (num %2 == 0) &&  (num>2) && (num<5){
+        System.out.println("Not Cool");
+    }
+if (num1%2 == 0) &&  (num>6) && (num<20) {
+        System.out.println("Cool)";
+}
+    if  (num1%2 == 0) &&  (num>20) {
+    System.out.println("Not Cool");
+}
+
+
+}
+
